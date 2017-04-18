4549cafab5af9b97a08f4b349d59f6e496c177a2
Nikita <nks.off@gmail.com>
2017-04-18 09:42:24 +0300

    add no covers

--- services/pg/schema.sql
+++ services/pg/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE songs(
 	id	SERIAL	NOT NULL PRIMARY KEY,
-	author 	varchar(100)	NOT NULL,
-	title 	varchar(100)	NOT NULL,
-	lyrics 	varchar(5000)	NOT NULL,
+	author 	text	NOT NULL,
+	title 	text	NOT NULL,
+	lyrics 	text	NOT NULL,
 	file_id	varchar(50)	NOT NULL,
 	genius_id int UNIQUE NOT NULL ,
 	created_at	timestamp with time zone default(now()));
@@ -12,5 +12,5 @@ CREATE TABLE transcription(
 	songId 	int REFERENCES songs,
 	start_time_ms	int NOT NULL,
 	end_time_ms	int NOT NULL,
-	phrase	varchar(250)	NOT NULL,
+	phrase	text	NOT NULL,
 	created_at	timestamp with time zone default(now()));
