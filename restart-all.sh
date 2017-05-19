e6f2f68c2c5c0110cf0109241a2bbeff89a7986b
SchadkoAO <schadko.ao@gmail.com>
2017-05-19 19:23:07 +0300

    fix typo

--- restart-all.sh
+++ restart-all.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo docker stop $(docker ps -a -q)
-
+sudo docker-compose stop
 sudo docker-compose build
 sudo docker-compose up -d
-
-sudo docker run -d -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer
