6aef94d6afc8a7760c4feae9f04b88625e06af01
SchadkoAO <schadko.ao@gmail.com>
2017-05-19 15:49:24 +0300

    fix command

--- restart-all.sh
+++ restart-all.sh
@@ -4,7 +4,7 @@ docker stop $(docker ps -a -q)
 
 git submodule update
 
-sudo docker-compose build .
-sudo docker-compose up -d .
+docker-compose build
+docker-compose up -d
 
 sudo docker run -d -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer
