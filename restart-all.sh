f71dd735b715c89ebac558a60b07ecd5069ff086
SchadkoAO <schadko.ao@gmail.com>
2017-05-19 15:46:34 +0300

    add restart all script

---
+++ restart-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker stop $(docker ps -a -q)
+
+git submodule update
+
+sudo docker-compose build .
+sudo docker-compose up -d .
+
+sudo docker run -d -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer
